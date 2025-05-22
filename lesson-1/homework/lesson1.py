dd7f77b8e01b1433e0f692774db95e06344bcb3b
MadinaMukhiddinovna <madinamukhiddinovnaa@gmail.com>
2025-05-22 22:16:10 +0500

    Update lesson1.py

--- lesson-1/homework/lesson1.py
+++ lesson-1/homework/lesson1.py
@@ -1,1 +1,49 @@
+#lesson1
+# Given a side of square. Find its perimeter and area.
+
+#given:
+side = 5 #side of the square 
+#calculating perimeter and area 
+perimeter = 4 * side 
+area = side ** 2 
+
+print("Perimeter of the square:", perimeter)
+print("Area of the square", area)
+
+# Given diameter of circle. Find its length.
+
+import math
+#given 
+diameter = 10 #diameter of the circle 
+
+#Calculating the circumference (length)
+length = math.pi * diameter
+print("Circumference of the circle:", length)
+
+# Given two numbers a and b. Find their mean.
+#given:
+a = 8
+b = 12
+
+#calculating mean
+mean = (a + b) / 2
+
+print("Mean of a and b:", mean)
+
+# Given two numbers a and b. Find their sum, product and square of each number.
+#given
+a = 7
+b = 3
+
+# Calculating sum, product and squares
+sum_ab = a + b
+product_ab = a * b
+square_a = a ** 2
+square_b = b ** 2
+
+print("sum of a and b:", sum_ab)
+print("product of a and b:", product_ab)
+print("square of a:", square_a)
+print("square of b:", square_b)
+
 
